84031a05ba224b8c3cad2a533d88cd4b3956b85e
Goshi <enepochatyy@gmail.com>
2017-10-12 00:10:21 +0300

    5

--- librip/ctxmngrs.py
+++ librip/ctxmngrs.py
@@ -1,3 +1,5 @@
+import time
+
 # Здесь необходимо реализовать
 # контекстный менеджер timer
 # Он не принимает аргументов, после выполнения блока он должен вывести время выполнения в секундах
@@ -6,3 +8,14 @@
 #   sleep(5.5)
 #
 # После завершения блока должно вывестись в консоль примерно 5.5
+
+
+class timer:
+    def __init__(self):
+        self.time = 0
+
+    def __enter__(self):
+        self.time = time.clock()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(time.clock() - self.time)
